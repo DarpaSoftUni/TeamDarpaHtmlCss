3d2c682b5a94e915ea298bcdaf00b4140f489d17
tpelev <tishkatap@gmail.com>
2015-12-27 14:33:10 +0200

    Project team Darpa
    Project so far

---
+++ scripts.js
@@ -0,0 +1,52 @@
+///**
+// * Created by Tihomir_Pelev on 23-Dec-15.
+// */
+//
+//
+//var divContainer;
+//var array = ["Plugins can be included individually", "Plugins can be included individually123"];
+//var counter = 0;
+//var snippet;
+//
+//$(document). (function() {
+//    var divContainer = $('#jokes-container');
+//
+//    for(var i=0; i<array.length; i ++ ) {
+//
+//        var snippet = "   " + " " + array[i] + "" + counter ;
+//        divContainer.append();
+//        counter ++;
+//    }
+//
+//
+//});
+//
+//
+//<button onlick="addJoke"><button>
+//
+//
+//
+//function addJoke() {
+//    var joke = $('#comment').val();
+//    var cont = $('#container');
+//    cont.append()
+//    counter++;
+//
+//}
+//
+//
+//
+//
+//var a = $('.container');
+//
+//
+//var myText = "teksta koito poluchavame ot input formata";
+//
+//
+//var snippet = " " + myText + " " + counter
+//
+//
+//
+//
+//a.attr('#');
+//
